976f00d9ca3d82e5b24ba014f2cbd641fbf1cf53
cps90 <cpswedenburg@gmail.com>
2018-04-20 09:27:04 -0600

    first commit

---
+++ warm-ups/quiz/quiz.js
@@ -0,0 +1,46 @@
+1. What are the three git commands we use to push our code to git everyday?
+
+      cd dev/assignments
+      git add -A
+      git commit -m “<your message>”
+      git push
+
+2. Fill in the blanks ( _ )
+
+    if (a === b) {
+      console.log(“a is equal to b”)
+    } else {
+      console.log("a and b are not equal")
+    };
+
+
+
+3. In CSS, how would we select all the HTML elements with the class “red”?
+
+    **.red {}**
+    #red {}
+    red {}
+    $red {}
+
+4. What would: “” === false, evaluate to?
+    **True**
+    False
+    Truthy
+    Falsey
+
+5. How would we print the soups 3rd ingredient to the console?
+
+    var soup = {
+      name: "Tomato",
+      ingredients: ["water", "salt", "tomato", "basil"]
+    };
+
+    console.log(soup.ingredients[2]);
+
+6. Fill in the blanks to list all the items in there array.
+
+    var coolCars = [“Entourage”, “E-150”, “Sprinter”];
+
+    for (var i = 0; i < coolCars.length; i++) {
+    console.log(coolCars[i]);
+    };
