56d2ee6216714d3c32c38c0b51ba93355b767e54
cps90 <cpswedenburg@gmail.com>
2018-04-26 14:59:54 -0600

    updates

--- projects/mario-pest-control/app.js
+++ projects/mario-pest-control/app.js
@@ -1,1 +1,11 @@
-                      
+
+
+document.baddiesForm.addEventListener("submit", function(e) {
+e.preventDefault()
+
+var goombaValue = document.baddiesForm.goombas.value
+var bobombValue = document.baddiesForm.bobombs.value
+var cheepCheepValue = document.baddiesForm.cheepCheeps.value
+document.getElementById('totalCoins').innerHTML = `The total value = ${(goombaValue * 5) + (bobombValue * 7) + (cheepCheepValue * 11)}`
+
+})
